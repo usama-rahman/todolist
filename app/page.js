7d89e9dd6fbf3cace9c1c841a0573eaf7b1f1479
Usama Rahman <rahmanusama390@gmail.com>
2023-10-15 22:14:44 +0600

    🍉

--- app/page.js
+++ app/page.js
@@ -4,12 +4,10 @@ const page = () =>{
   return (
     <>
     <div className="w-full h-screen block justify-center items-center">
-      <h1 className="text-2xl text-center my-5">
-        usama's ToDo List
-      </h1>
+      <h1 className="text-2xl text-center my-10">Usama's ToDo List</h1>
       <from className="w-full flex justify-center my-5">
         <input type="text" className="text rounded bg-slate-200 px-3 py-1" placeholder="Enter Task"></input>
-        <button className="">Add Task</button>
+        <button className="px-4 bg-slate-800 rounded mx-3">Add Task</button>
       </from>
     </div>
     </>
