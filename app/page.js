334f71806fe7fedd61bd6417733f457afa9e2ee5
Usama Rahman <rahmanusama390@gmail.com>
2023-10-15 22:10:24 +0600

    Working

--- app/page.js
+++ app/page.js
@@ -3,9 +3,15 @@ import React from "react";
 const page = () =>{
   return (
     <>
+    <div className="w-full h-screen block justify-center items-center">
       <h1 className="text-2xl text-center my-5">
         usama's ToDo List
       </h1>
+      <from className="w-full flex justify-center my-5">
+        <input type="text" className="text rounded bg-slate-200 px-3 py-1" placeholder="Enter Task"></input>
+        <button className="">Add Task</button>
+      </from>
+    </div>
     </>
   )
 }
