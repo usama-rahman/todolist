12241db5df79ecb8b8166bb829b0042c666aec38
Usama Rahman <rahmanusama390@gmail.com>
2023-10-15 21:53:56 +0600

    working 👍

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () =>{
+  return (
+    <>
+      <h1 className="text-2xl text-center my-5">
+        usama's ToDo List
+      </h1>
+    </>
+  )
+}
+
+export default page
