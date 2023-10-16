0352d0165189568b70624c523899b450c1acaa07
Usama Rahman <rahmanusama390@gmail.com>
2023-10-16 08:47:46 +0600

    Working

--- app/page.js
+++ app/page.js
@@ -1,26 +1,42 @@
 "use client"
+import { Cagliostro, Climate_Crisis } from "next/font/google";
 import React,{useState} from "react";
 
 const page = () =>{
   const [title, setTitle] = useState("");
   const [discription, setDiscription] = useState("");
+  const submitHandler = (e) =>{
+    e.preventDefult()
+    console.log(title);
+    console.log(discription);
+    setTitle("")
+    setDiscription("")
+  }
   return (
     <>
     <div className="w-full h-screen block justify-center items-center">
       <h1 className="text-2xl text-center my-10">Usama's ToDo List</h1>
-      <from className="w-full flex justify-center my-5">
+      <form className="w-full flex justify-center my-5" onSubmit={submitHandler}>
         <input 
-        type="text" className="text rounded bg-slate-200 px-3 py-1 mx-3" placeholder="Enter Title" value={title}>
+          type="text" 
+          className="text rounded bg-slate-200 px-3 py-1 mx-3 text-slate-800" 
+          placeholder="Enter Title" 
+          value={title}
           onChange={(e) => {
             setTitle(e.target.value)
           }}
-        </input>
+        />
         <input 
-        type="text" className="text rounded bg-slate-200 px-3 py-1 mx-3" placeholder="Enter Discription" value={discription}>
-
-        </input>
+          type="text" 
+          className="text rounded bg-slate-200 px-3 py-1 mx-3 text-slate-700" 
+          placeholder="Enter Discription" 
+          value={discription}
+          onChange={(e) => {
+            setDiscription(e.target.value)
+          }}
+        />
         <button className="px-4 bg-slate-800 rounded mx-3">Add Task</button>
-      </from>
+      </form>
     </div>
     </>
   )
